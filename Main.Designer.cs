01640af9944a4466b3f69d60ca99317912a63335
FauxSeal210 <vkdnj8084@naver.com>
2020-03-28 20:49:25 +0900

    First Commit

---
+++ Main.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TwitchVODownloader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkBox1080p60 = new System.Windows.Forms.RadioButton();
+            this.chkBox720p60 = new System.Windows.Forms.RadioButton();
+            this.chkBox720p = new System.Windows.Forms.RadioButton();
+            this.chkBox480p = new System.Windows.Forms.RadioButton();
+            this.chkBox360p = new System.Windows.Forms.RadioButton();
+            this.chkBox160p = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVideosize = new System.Windows.Forms.Label();
+            this.txtBoxPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBoxLink
+            // 
+            this.txtBoxLink.Location = new System.Drawing.Point(12, 38);
+            this.txtBoxLink.Name = "txtBoxLink";
+            this.txtBoxLink.Size = new System.Drawing.Size(522, 21);
+            this.txtBoxLink.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "다시보기 영상 주소";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(312, 220);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(222, 41);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "다운로드";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkBox1080p60);
+            this.groupBox1.Controls.Add(this.chkBox720p60);
+            this.groupBox1.Controls.Add(this.chkBox720p);
+            this.groupBox1.Controls.Add(this.chkBox480p);
+            this.groupBox1.Controls.Add(this.chkBox360p);
+            this.groupBox1.Controls.Add(this.chkBox160p);
+            this.groupBox1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "화질";
+            // 
+            // chkBox1080p60
+            // 
+            this.chkBox1080p60.AutoSize = true;
+            this.chkBox1080p60.Location = new System.Drawing.Point(373, 26);
+            this.chkBox1080p60.Name = "chkBox1080p60";
+            this.chkBox1080p60.Size = new System.Drawing.Size(90, 20);
+            this.chkBox1080p60.TabIndex = 5;
+            this.chkBox1080p60.Text = "1080p60";
+            this.chkBox1080p60.UseVisualStyleBackColor = true;
+            // 
+            // chkBox720p60
+            // 
+            this.chkBox720p60.AutoSize = true;
+            this.chkBox720p60.Checked = true;
+            this.chkBox720p60.Location = new System.Drawing.Point(286, 26);
+            this.chkBox720p60.Name = "chkBox720p60";
+            this.chkBox720p60.Size = new System.Drawing.Size(81, 20);
+            this.chkBox720p60.TabIndex = 4;
+            this.chkBox720p60.TabStop = true;
+            this.chkBox720p60.Text = "720p60";
+            this.chkBox720p60.UseVisualStyleBackColor = true;
+            // 
+            // chkBox720p
+            // 
+            this.chkBox720p.AutoSize = true;
+            this.chkBox720p.Location = new System.Drawing.Point(217, 26);
+            this.chkBox720p.Name = "chkBox720p";
+            this.chkBox720p.Size = new System.Drawing.Size(63, 20);
+            this.chkBox720p.TabIndex = 3;
+            this.chkBox720p.Text = "720p";
+            this.chkBox720p.UseVisualStyleBackColor = true;
+            // 
+            // chkBox480p
+            // 
+            this.chkBox480p.AutoSize = true;
+            this.chkBox480p.Location = new System.Drawing.Point(147, 26);
+            this.chkBox480p.Name = "chkBox480p";
+            this.chkBox480p.Size = new System.Drawing.Size(64, 20);
+            this.chkBox480p.TabIndex = 2;
+            this.chkBox480p.Text = "480p";
+            this.chkBox480p.UseVisualStyleBackColor = true;
+            // 
+            // chkBox360p
+            // 
+            this.chkBox360p.AutoSize = true;
+            this.chkBox360p.Location = new System.Drawing.Point(78, 26);
+            this.chkBox360p.Name = "chkBox360p";
+            this.chkBox360p.Size = new System.Drawing.Size(63, 20);
+            this.chkBox360p.TabIndex = 1;
+            this.chkBox360p.Text = "360p";
+            this.chkBox360p.UseVisualStyleBackColor = true;
+            // 
+            // chkBox160p
+            // 
+            this.chkBox160p.AutoSize = true;
+            this.chkBox160p.Location = new System.Drawing.Point(9, 26);
+            this.chkBox160p.Name = "chkBox160p";
+            this.chkBox160p.Size = new System.Drawing.Size(63, 20);
+            this.chkBox160p.TabIndex = 0;
+            this.chkBox160p.Text = "160p";
+            this.chkBox160p.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "예상되는 파일 용량:";
+            // 
+            // lblVideosize
+            // 
+            this.lblVideosize.AutoSize = true;
+            this.lblVideosize.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblVideosize.Location = new System.Drawing.Point(241, 229);
+            this.lblVideosize.Name = "lblVideosize";
+            this.lblVideosize.Size = new System.Drawing.Size(51, 16);
+            this.lblVideosize.TabIndex = 5;
+            this.lblVideosize.Text = "None";
+            // 
+            // txtBoxPath
+            // 
+            this.txtBoxPath.Location = new System.Drawing.Point(12, 97);
+            this.txtBoxPath.Name = "txtBoxPath";
+            this.txtBoxPath.ReadOnly = true;
+            this.txtBoxPath.Size = new System.Drawing.Size(467, 21);
+            this.txtBoxPath.TabIndex = 7;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(485, 97);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(49, 23);
+            this.btnPath.TabIndex = 8;
+            this.btnPath.Text = "...";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "영상이 저장될 폴더";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 291);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtBoxPath);
+            this.Controls.Add(this.lblVideosize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxLink);
+            this.Name = "Main";
+            this.Text = "VOD Downloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton chkBox1080p60;
+        private System.Windows.Forms.RadioButton chkBox720p60;
+        private System.Windows.Forms.RadioButton chkBox720p;
+        private System.Windows.Forms.RadioButton chkBox480p;
+        private System.Windows.Forms.RadioButton chkBox360p;
+        private System.Windows.Forms.RadioButton chkBox160p;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVideosize;
+        private System.Windows.Forms.TextBox txtBoxPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitchVODownloader
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            ValidateLinks();
+            CheckAndCreateDirectory();        
+
+            string resolution = GetResolution();
+
+            if (MessageBox.Show("정말 " + resolution + " 화질의 영상을 다운로드 하시겠습니까?", "알림", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            StartDownload();                             
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    txtBoxPath.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void ValidateLinks()
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxPath.Text))
+            {
+                MessageBox.Show("비디오 파일 경로가 입력되어 있는지 확인해주세요.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtBoxLink.Text))
+            {
+                MessageBox.Show("다시보기 링크가 입력되어 있는지 확인해주세요.");
+                return;
+            }
+        }
+
+        private void CheckAndCreateDirectory()
+        {
+            string data_folder_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TwitchVODownloader");
+
+            if (!Directory.Exists(data_folder_path))
+                Directory.CreateDirectory(data_folder_path);
+        }
+
+        private string GetResolution()
+        {
+            string resolution = "";
+
+            if (chkBox160p.Checked)
+            {
+                resolution = "160p30";
+            }
+
+            else if (chkBox360p.Checked)
+            {
+                resolution = "360p30";
+            }
+
+            else if (chkBox480p.Checked)
+            {
+                resolution = "480p30";
+            }
+
+            else if (chkBox720p.Checked)
+            {
+                resolution = "720p30";
+            }
+
+            else if (chkBox720p60.Checked)
+            {
+                resolution = "720p60";
+            }
+
+            else if (chkBox1080p60.Checked)
+            {
+                resolution = "1080p60";
+            }
+
+            return resolution;
+        }
+
+        private void StartDownload()
+        {
+            string resolution = GetResolution();
+            string data_folder_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TwitchVODownloader");
+            string video_list_path = Path.Combine(data_folder_path, "video_list");
+
+            DownloadVideoListFile(video_list_path);
+            QueueAndDownloadTSFiles(resolution, data_folder_path, video_list_path);
+        }
+
+        private void DownloadVideoListFile(string video_list_path)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(txtBoxLink.Text.Trim(), video_list_path);
+            }
+        }
+
+        private void QueueAndDownloadTSFiles(string resolution, string data_folder_path, string video_list_path)
+        {
+            string[] lines = File.ReadAllLines(video_list_path);
+            string vodUrl;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("#"))
+                    continue;
+
+                if (!line.Contains("cloudfront.net") && line.Contains("vod-secure.twitch.tv"))
+                {
+                    MessageBox.Show("DBG");
+                    DownloadVideoListFile(video_list_path);
+                    QueueAndDownloadTSFiles(resolution, data_folder_path, video_list_path);
+                    return;
+                }
+
+                if (line.Contains(resolution))
+                {
+                    string sub = line.Substring(0, line.LastIndexOf('/'));
+                    vodUrl = line.Substring(0, sub.LastIndexOf('/') + 1) + resolution + '/';
+
+                    string video_info_path = Path.Combine(data_folder_path, "video_info");
+
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(line.Trim(), video_info_path);
+                    }
+
+                    string[] video_info = File.ReadAllLines(video_info_path);
+                    string lastFile = video_info[video_info.Length - 2].Replace("-muted", "");
+                    int lastFileIndex = int.Parse(lastFile.Substring(0, lastFile.IndexOf('.')).Trim()) + 1;
+
+                    List<Task> tasks = new List<Task>();
+
+                    for (int i = 0; i < lastFileIndex; i++)
+                    {
+                        using (var client = new WebClient())
+                        {
+                            tasks.Add(client.DownloadFileTaskAsync(new Uri(vodUrl + i + ".ts"), Path.Combine(txtBoxPath.Text, i + ".ts")));
+                        }
+                    }
+
+                    DownloadTSFilesAsync(tasks);
+
+                    break;
+                }
+            }
+        }
+        
+
+        private async void DownloadTSFilesAsync(IEnumerable<Task> tasks)
+        {
+            await Task.WhenAll(tasks);
+            CompressTSFiles(tasks.Count());           
+        }
+
+        private void CompressTSFiles(int lastFileIndex)
+        {
+            MessageBox.Show("필요한 파일들을 다운로드 했습니다. 이제 합치기 작업을 시작합니다.");
+
+            string video_folder_path = txtBoxPath.Text;
+            string tsFilesList = "";
+
+            Process process;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.UseShellExecute = true;
+            processStartInfo.WorkingDirectory = Path.GetFullPath(video_folder_path);
+
+            string argument = "";
+
+            if (lastFileIndex > 300)
+            {
+                List<string> tmpCompressedList = new List<string>();
+
+                int part = lastFileIndex / 300;
+
+                for (int j = 0; j < part + 1; j++)
+                {
+                    for (int i = (j * 300); i < (j * 300) + 300; i++)
+                    {
+                        if (tsFilesList.Equals(""))
+                            tsFilesList = i + ".ts";
+
+                        else
+                            tsFilesList += "+" + i + ".ts";
+
+                        if (i == lastFileIndex || i == (j * 300) + 299)
+                        {
+                            argument = "/c copy /b " + tsFilesList + " " + GetColumnName(j) + ".ts";
+
+                            processStartInfo.Arguments = argument;
+
+                            process = Process.Start(processStartInfo);
+                            process.WaitForExit();
+
+                            tsFilesList = "";                          
+
+                            tmpCompressedList.Add(GetColumnName(j));
+
+                            if (i == lastFileIndex)
+                                break;
+                        }                    
+                    }                  
+                }
+
+                tsFilesList = "";
+
+                foreach (string letter in tmpCompressedList)
+                {
+                    if (tsFilesList.Equals(""))
+                        tsFilesList = "a.ts";
+
+                    else
+                        tsFilesList += "+" + letter + ".ts";
+                }
+
+                argument = "/c copy /b " + tsFilesList + " combined.ts";
+
+                Debug.WriteLine("argument : " + argument);
+
+                processStartInfo.Arguments = argument;
+
+                process = Process.Start(processStartInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("합치기 작업을 완료하였습니다.");
+            }
+
+            else
+            {
+                tsFilesList = "0.ts";
+
+                for (int i = 1; i < lastFileIndex; i++)
+                {
+                    tsFilesList += "+" + i + ".ts";
+                }
+
+                argument = "/c copy /b " + tsFilesList + " combined.ts";
+
+                Debug.WriteLine("argument : " + argument);
+
+                processStartInfo.Arguments = argument;
+
+                process = Process.Start(processStartInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("합치기 작업을 완료하였습니다.");
+            }
+
+            foreach (FileInfo file in new DirectoryInfo(video_folder_path).GetFiles("*.ts"))
+            {
+                if (file.Name.Equals("combined.ts"))
+                    continue;
+
+                file.Delete();
+            }
+
+
+        }
+
+        private string GetColumnName(int index)
+        {
+            const string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            string value = "";
+
+            if (index >= letters.Length)
+                value += letters[index / letters.Length - 1];
+
+            value += letters[index % letters.Length];
+
+            return value;
+        }
+    }
+}
